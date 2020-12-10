8bb55780aacb6d7f12310f9c460f439289fb99d4
pierrick.delort <pk_del@outlook.fr>
2020-12-11 00:31:44 +0100

    debut interface graphique mais nsm

--- Graphic/graphic.c
+++ Graphic/graphic.c
@@ -1,38 +1,63 @@
-#include <SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <gtk/gtk.h>
 
-void window(){
-	SDL_INIT(SDL_INIT_VIDEO);
 
-	SDL_Window *window = NULL;
-	window = SDL_CreateWindow("Optical Character Recognition (Graphic)",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,0);
 
-	SDL_Renderer *renderer = NULL;
-	renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_SOFTWARE);
+int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder; 
+    GtkWidget       *window;
+    GdkColor color ;
+    gdk_color_parse( "#0080FF", &color );
 
-	SDL_bool launched = SDL_True;
+    gtk_init(&argc, &argv);
 
-	while(launched){
-		SDL_Event event;
+    builder = gtk_builder_new_from_file("glade/Menu.glade");
 
-		while(SDL_PollEvent(&event)){
-			switch(event.type){
-				case SDL_QUIT:
-				     launched = SDL_False;
-				     break;
-				default:
-				     break;
-			}
-		}
-	}
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "Menu"));
+    gtk_widget_override_background_color(window, GTK_STATE_NORMAL, &color);
+    
+    gtk_builder_connect_signals(builder, NULL);
 
-	SDL_RenderPresent(renderer);
-	
+    g_object_unref(builder);
 
-	
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_QUIT();
+    gtk_widget_show(window);                
+    gtk_main();
 
+    return 0;
+}
+
+// called when window is closed
+void on_Menu_destroy()
+{
+    gtk_main_quit();
+}
+
+void on_Training_Button_clicked(){
+    GtkBuilder *builder;
+    GtkWidget  *Training;
+    builder = gtk_builder_new_from_file("glade/Training.glade");
+    Training = GTK_WIDGET(gtk_builder_get_object(builder, "Training"));
+    gtk_builder_connect_signals(builder, NULL);
+    g_object_unref(builder);
+    gtk_widget_show(Training);
+}
+
+void on_Manual_Button_clicked(){
+    GtkBuilder *builder;
+    GtkWidget  *Manual;
+    builder = gtk_builder_new_from_file("glade/Manual.glade");
+    Manual = GTK_WIDGET(gtk_builder_get_object(builder, "Manual"));
+    gtk_builder_connect_signals(builder, NULL);
+    g_object_unref(builder);
+    gtk_widget_show(Manual);
+}
+
+void on_Automatic_Button_clicked(){
+    GtkBuilder *builder;
+    GtkWidget  *Automatic;
+    builder = gtk_builder_new_from_file("glade/Automatic.glade");
+    Automatic = GTK_WIDGET(gtk_builder_get_object(builder, "Automatic"));
+    gtk_builder_connect_signals(builder, NULL);
+    g_object_unref(builder);
+    gtk_widget_show(Automatic);
 }
