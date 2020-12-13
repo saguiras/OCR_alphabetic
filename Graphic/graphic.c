5bbe90ef52a0478588d4933556cd8f6cea72e05d
pierrick.delort <pk_del@outlook.fr>
2020-12-13 03:10:38 +0100

    plus qu a finir les connections aux fonctions et fini

--- Graphic/graphic.c
+++ Graphic/graphic.c
@@ -1,22 +1,20 @@
 #include <gtk/gtk.h>
+#include "../neural_network/neural_Tools.h"
+#include "../tools/tools.h"
 
-
+GtkWidget *loading_spin ;
 
 int main(int argc, char *argv[])
 {
     GtkBuilder      *builder; 
     GtkWidget       *window;
-    GdkColor color ;
-    gdk_color_parse( "#0080FF", &color );
-
-    gtk_init(&argc, &argv);
 
     builder = gtk_builder_new_from_file("glade/Menu.glade");
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "Menu"));
-    gtk_widget_override_background_color(window, GTK_STATE_NORMAL, &color);
     
     gtk_builder_connect_signals(builder, NULL);
+    loading_spin = GtK_WIDGET(gtk_builder_get_object(builder, "loading_spin"));
 
     g_object_unref(builder);
 
@@ -33,31 +31,59 @@ void on_Menu_destroy()
 }
 
 void on_Training_Button_clicked(){
-    GtkBuilder *builder;
-    GtkWidget  *Training;
-    builder = gtk_builder_new_from_file("glade/Training.glade");
-    Training = GTK_WIDGET(gtk_builder_get_object(builder, "Training"));
-    gtk_builder_connect_signals(builder, NULL);
-    g_object_unref(builder);
-    gtk_widget_show(Training);
+    gtk_spinner_start(loading_spin);
+    Learn_NN();
+    gtk_spinner_stop(loading_spin);
 }
 
 void on_Manual_Button_clicked(){
     GtkBuilder *builder;
     GtkWidget  *Manual;
+    GtkWidget *blur_factor;
+    GtkWidget *bright_factor;
     builder = gtk_builder_new_from_file("glade/Manual.glade");
     Manual = GTK_WIDGET(gtk_builder_get_object(builder, "Manual"));
+    blur_factor = GTK_WIDGET(gtk_builder_get_object(builder, "blur_entry"));
+    bright_factor = GTK_WIDGET(gtk_builder_get_object(builder, "bright_entry"));
     gtk_builder_connect_signals(builder, NULL);
     g_object_unref(builder);
     gtk_widget_show(Manual);
 }
 
+//functions for manual
+void on_BnW_clicked(){
+    //call fction bnw on image
+}
+
+void on_Greyscale_clicked(){
+
+}
+
+void on_Blur_clicked(){
+
+}
+
+void on_Brightness_clicked(){
+
+}
+
+void on_neural_launch_clicked(){
+    
+}
+
 void on_Automatic_Button_clicked(){
     GtkBuilder *builder;
     GtkWidget  *Automatic;
+    GtkWidget *file_chooser;
     builder = gtk_builder_new_from_file("glade/Automatic.glade");
     Automatic = GTK_WIDGET(gtk_builder_get_object(builder, "Automatic"));
+    file_chooser = GTK_WIDGET(gtk_builder_get_object(builder,"auto_file"));
     gtk_builder_connect_signals(builder, NULL);
     g_object_unref(builder);
     gtk_widget_show(Automatic);
 }
+
+void on_launch_automatic_clicked(){
+    //faire les import et appeler ttes les fonctions
+}
+
